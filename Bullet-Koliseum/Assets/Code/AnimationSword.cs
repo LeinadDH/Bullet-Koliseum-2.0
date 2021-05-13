5108e9b28d3e15d20ac0bcc8ca7f2bb046cf55c3
Daniel Navarrete <daniel_navarrete_05@hotmail.com>
2021-05-13 02:37:55 -0500

    DoTween rotation

---
+++ Bullet-Koliseum/Assets/Code/AnimationSword.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class AnimationSword : MonoBehaviour
+{
+    public Vector3 rotate;
+    RotateMode mode = RotateMode.Fast;
+    public float flip;
+    public int Loops;
+    public LoopType loopType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DORotate(rotate, flip, mode).SetLoops(Loops, loopType);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
