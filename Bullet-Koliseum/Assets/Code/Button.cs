8ecf072aeed41ccceefb598d1a54cd8d742493e5
Daniel <cdmx1963@amerike.edu.mx>
2021-05-14 16:10:52 -0500

    Listo 3er parcial :D
    Menu funcional con slider de musica

--- Bullet-Koliseum/Assets/Code/Button.cs
+++ Bullet-Koliseum/Assets/Code/Button.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class Button : MonoBehaviour
 {
     public GameObject Menu;
+    public GameObject MusicMenu;
     public void Continue()
     {
         Time.timeScale = 1;
         Menu.SetActive(false);
+    }
+    public void ShowMusic()
+    {
+        Menu.SetActive(false);
+        MusicMenu.SetActive(true);
+    }
+    public void ExitMusic()
+    {
+        Menu.SetActive(true);
+        MusicMenu.SetActive(false);
     }
 }
