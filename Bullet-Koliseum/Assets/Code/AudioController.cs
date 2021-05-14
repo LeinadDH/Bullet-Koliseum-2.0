347502811d004b23c3cb7633a62e96c12e495737
Daniel <cdmx1963@amerike.edu.mx>
2021-05-14 12:45:57 -0500

    Audio complete
    Audio Mixer manejable con una canción en loop

---
+++ Bullet-Koliseum/Assets/Code/AudioController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using TMPro;
+
+public class AudioController : MonoBehaviour
+{
+    const string musicVolume = "Music Volume";
+    public AudioMixer audioMixer;
+    [Space]
+    public Slider musicSilder;
+    public TextMeshProUGUI musicTXT;
+    private void Awake()
+    {
+        musicSilder.minValue = 0.001f;
+        musicSilder.maxValue = 1;
+        musicSilder.onValueChanged.AddListener(SetMusicVolume);
+
+        musicSilder.value = PlayerPrefs.GetFloat(musicVolume, 1);
+    }
+    public void SetMusicVolume(float v)
+    {
+        audioMixer.SetFloat(musicVolume, Mathf.Log10(v) * 20);
+        musicTXT?.SetText(Mathf.RoundToInt(v * 100).ToString() + " %");
+        PlayerPrefs.SetFloat(musicVolume, v);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
