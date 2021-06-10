44cf019dcc98d58bd63fd08c46335b4ae89cc7ca
Daniel Navarrete <daniel_navarrete_05@hotmail.com>
2021-06-10 17:52:58 -0500

    Multiplayer menu

---
+++ Bullet-Koliseum/Assets/Multiplayer/Code/PlayerConfigurationManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class PlayerConfigurationManager : MonoBehaviour
+{
+    private List<PlayerConfiguration> playerConfigs;
+
+    [SerializeField]
+    private int MaxPlayers = 2;
+
+    public static PlayerConfigurationManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Debug.Log("SINGLETON - Trying to create another instance of singleton");
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(Instance);
+            playerConfigs = new List<PlayerConfiguration>();
+        }
+    }
+    
+    public void SetPlayerPrefab(int index, GameObject player)
+    {
+        playerConfigs[index].playerPrefab = gameObject;
+    }
+
+    public void ReadyPlayer(int index)
+    {
+        playerConfigs[index].IsReady = true;
+        if(playerConfigs.Count == MaxPlayers && playerConfigs.All(p => p.IsReady == true))
+        {
+            SceneManager.LoadScene("MonaChina");
+        }
+    }
+
+    public void HandlePlayerJoin(PlayerInput pi)
+    {
+        Debug.Log("Player Join" + pi.playerIndex);
+        pi.transform.SetParent(transform);
+        if(!playerConfigs.Any(p => p.PlayerIndex == pi.playerIndex))
+        {
+            playerConfigs.Add(new PlayerConfiguration(pi));
+        }
+    }
+}
+
+public class PlayerConfiguration
+{
+    public PlayerConfiguration(PlayerInput pi)
+    {
+        PlayerIndex = pi.playerIndex;
+        Input = pi;
+    }
+
+    public PlayerInput Input { get; set; }
+
+    public int PlayerIndex { get; set; }
+
+    public bool IsReady { get; set; }
+
+    public GameObject playerPrefab { get; set; }
+}
